3420eb5e4bcb79c1f37484188976de4b54ecf0b7
Sadhana <155810661+Sadhana05@users.noreply.github.com>
2024-10-11 22:12:14 +0530

    second commit

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -8,7 +8,7 @@ const ShopCategory = (props) => {
   const {all_product} = useContext (ShopContext);
   return (
     <div className='shop-category'>
-      <img src={props.banner} alt="" />
+      <img className='shopcategory-banner' src={props.banner} alt="" />
       <div className="shopcategory-indexSort">
         <p>
           <span>Showing 1-12</span> out of 36 products
@@ -19,13 +19,16 @@ const ShopCategory = (props) => {
       </div>
       <div className="shopcategory-products">
         {all_product.map((item,i)=>{
-          if (props.category===Item.category) {
+          if (props.category===item.category) {
             return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
           }
           else {
             return null;
           }
         })}
+      </div>
+      <div className="shopcategory-loadmore">
+        Explore More
       </div>
     </div>
   )
